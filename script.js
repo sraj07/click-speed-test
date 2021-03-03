fb87149f0d330b30691bb8e8d3c5c931c1a1fb9b
Sudev-the-dev <74224035+Sudev-the-dev@users.noreply.github.com>
2021-03-02 20:03:43 -0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,34 @@
+var clicks = 0;
+var timer = 5;
+    
+$("#result").hide();
+    
+function start(){
+  setTimeout(tup, 5000);
+  t = setInterval(detim, 1000);
+  $("#btn").attr("onclick", "scadd()");
+  $("#btn").html("Click");
+  $("#time").html("Time left: 5")
+}
+    
+function scadd(){ // Score Add
+  clicks++;
+  $("#score").text("Clicks: " + clicks);
+}
+    
+function tup(){ // Time Up
+  var cps = clicks / 5;
+  $("#time").html("Your CPS was: " + cps);
+  $("#btn").attr("onclick", "donth()");
+  $("#btn").html("Finished");
+  clearInterval(t);
+}
+    
+function detim(){ // Decrease Time
+  timer -= 1;
+  $("#time").html("Time left: " + timer);
+}
+    
+function donth(){ // Do Nothing
+      
+}
